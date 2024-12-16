787093bda8f285266cfaf7413b2e8d53a0d6f4e8
DCSon-VN11 <sondinhcong11@gmail.com>
2024-12-16 12:51:49 +0700

    Xóa tệp khỏi Git nhưng giữ lại trên máy tính

--- resources/views/pagetitle.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="d-sm-flex align-items-center justify-content-between page-title">
-    <h4 class="mb-sm-0" style="font-size: 18px;">{{ $title }}</h4>
-    <div style="font-size: 14px">
-        {{ $titlesm }} / {{ $page }}
-    </div>
-</div>
